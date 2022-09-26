75ce02b8f81b5b797fdcd2ed1870c096aa9f5cea
Sergio Nizama <s.nizama@gmail.com>
2022-09-27 04:00:42 +0900

    added @types/webxr

--- src/app.tsx
+++ src/app.tsx
@@ -4,12 +4,6 @@ import AsyncRoute from 'preact-async-route'
 import PromptScreen from 'components/PromptScreen'
 import LoadingPrompt from 'components/LoadingPrompt'
 
-declare global {
-  interface Navigator {
-    xr: any
-  }
-}
-
 export function App() {
   return (
     <div class='w-screen h-screen'>
